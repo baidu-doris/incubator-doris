e333b7409a68b213f78818b73ea861aa2042bb8c
jiangshouzhuang <jiangshouzhuang@163.com>
2019-08-11 14:19:31 +0800

    fix backends disk error statistics issue

--- fe/src/test/java/org/apache/doris/common/util/UnitTestUtil.java
+++ fe/src/test/java/org/apache/doris/common/util/UnitTestUtil.java
@@ -128,11 +128,11 @@ public class UnitTestUtil {
     }
 
     public static Backend createBackend(long id, String host, int heartPort, int bePort, int httpPort,
-                                        long totalCapacityB, long disk_total_capacity, long availableCapacityB) {
+                                        long totalCapacityB, long diskTotalCapacityB, long availableCapacityB) {
         Backend backend = createBackend(id, host, heartPort, bePort, httpPort);
         Map<String, TDisk> backendDisks = new HashMap<String, TDisk>();
         String rootPath = "root_path";
-        TDisk disk = new TDisk(rootPath, totalCapacityB, disk_total_capacity, availableCapacityB, true);
+        TDisk disk = new TDisk(rootPath, totalCapacityB, diskTotalCapacityB, availableCapacityB, true);
         backendDisks.put(rootPath, disk);
         backend.updateDisks(backendDisks);
         return backend;
