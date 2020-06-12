c25ff9a09480a6d5eac2a2b2d819aef48eaf9f65
emmymiao87 <522274284@qq.com>
2020-06-12 11:43:34 +0800

    Support bitmap_union, hll_union and count when creating and matching a materialized view.
    This commit mainly supports creating bitmap_union, hll_union, and count materialized views.
    * The main changes are as follows:
    1. When creating a materialized view, doris judge the semantic analysis of the newly supported aggregate function.
    Only bitmap_union(to_bitmap(column)), hll_union(hll_hash(column)) and count(column) are supported.
    
    2. Match the correct materialized view when querying.
    After the user sends the query, if there is a possibility of matching the materialized view, the query will be rewritten firstly.
        Such as:
        Table: k1 int, k2 int
        MV: k1 int, mv_bitmap_union_k2 bitmap mv_bitmap_union
            mv_bitmap_union = to_bitmap(k2)
        Query: select k1, count(distinct k2) from Table
        Found that there is a match between the materialized view column and the query column, the query is rewritten as:
        Rewritten query: select k1, bitmap_union_count(mv_bitmap_union_k2) from table
    
    Then when the materialized view is matched, it can be matched to the query materialized view table.
    Sometimes the rewritten query may not match any materialized view, which means that the rewriting failed. The query needs to be re-parsed and executed again.
    
    Change-Id: I96a7522c4e6a58150b0df1886cf22c895c1c2bad

--- fe/src/main/java/org/apache/doris/alter/RollupJobV2.java
+++ fe/src/main/java/org/apache/doris/alter/RollupJobV2.java
@@ -307,6 +307,7 @@ public class RollupJobV2 extends AlterJobV2 implements GsonPostProcessable {
 
         tbl.setIndexMeta(rollupIndexId, rollupIndexName, rollupSchema, 0 /* init schema version */,
                 rollupSchemaHash, rollupShortKeyColumnCount,TStorageType.COLUMN, rollupKeysType, origStmt);
+        tbl.rebuildFullSchema();
     }
 
     /**
